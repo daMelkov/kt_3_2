037765ceea88a04b5806ecb80ca6964023659841
Melkov Denis <d.a.melkov@gmail.com>
2021-01-09 23:31:10 +0300

    add empty tests

---
+++ src/test/kotlin/CommentServiceTest.kt
@@ -0,0 +1,34 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class CommentServiceTest {
+
+    @Test
+    fun add() {
+    }
+
+    @Test
+    fun delete() {
+    }
+
+    @Test
+    fun edit() {
+    }
+
+    @Test
+    fun read() {
+    }
+
+    @Test
+    fun getById() {
+    }
+
+    @Test
+    fun restore() {
+    }
+
+    @Test
+    fun testRead() {
+    }
+}
